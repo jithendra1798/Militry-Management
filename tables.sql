33ca421e8f9856a2eb911e7ded8eb7415af478a3
jithendra1798 <siddartha1798@gmail.com>
2021-03-10 11:10:48 +0530

    Edited some table names

--- tables.sql
+++ tables.sql
@@ -14,7 +14,7 @@ CREATE TABLE LOCATION (
 
 
 CREATE TABLE BATALLIAN (
-	batallion_name varchar(50) not null primary key,
+	batallion_name varchar(50) not null PRIMARY KEY,
 	captain_id int not null,
 	year int not null,
 	total_capacity int not null
@@ -22,12 +22,13 @@ CREATE TABLE BATALLIAN (
 	
 	
 CREATE TABLE SOLDIER (
-	id int not null primary key,
+	id int not null PRIMARY KEY,
 	name varchar(50) not null,
 	doj DATE not null,
-	squadno varchar(50) not null references BATALLIAN(batallion_name),
-	birth_pincode int not null references LOCATION(pincode),
+	squadno varchar(50) not null references BATALLIAN(batallion_name) on update cascade on delete cascade,
+	birth_pincode int not null references LOCATION(pincode) on update cascade on delete cascade,
 	height int not null,
+	soldier_rank varchar(15) not null,
 	weight int not null,
 	chest int not null,
 	sex char(1) not null check(sex='M' or sex='F'),
@@ -37,30 +38,36 @@ CREATE TABLE SOLDIER (
 
 
 CREATE TABLE VISITED (
-	soldier_id int not null references SOLDIER(id),
-	pincode int not null references LOCATION(pincode),
+	soldier_id int not null references SOLDIER(id) on update cascade on delete cascade,
+	pincode int not null references LOCATION(pincode) on update cascade on delete cascade,
 	reason varchar(200) not null,
 	visit_date DATE not null
 	);
 
+
 CREATE TABLE MEDAL (
-	name varchar(50) not null primary key
+	name varchar(50) not null PRIMARY KEY
 	);
 	
+	
 CREATE TABLE REWARD (
-	name varchar(50) not null references MEDAL(name),
-	id int not null references SOLDIER(id),
+	name varchar(50) not null references MEDAL(name) on update cascade on delete cascade,
+	id int not null references SOLDIER(id) on update cascade on delete cascade,
 	year int not null check(year>=1000 and year<=5000)
 	);
 	
-create table manufacturing(
+
+
+	
+	
+create table MANUFACTURING(
 w_id int not null,
 manufacturing_date date not null,
 manufacturing_location varchar(50) not null,
 name varchar(50) not null
 );
 create table COMPANY(
-name varchar(50) not null references manufacturing(name),
+name varchar(50) not null references MANFACTURING(name),
 class varchar(50) null
 );
 
@@ -80,11 +87,73 @@ primary key(name)
  class varchar(50) not null
  );
 	
+
+create TABLE WORK (
+type   varchar(80) primary key,
+salary int not null
+);
+
+create table ASSIGN(
+  sold_id int  references SOLDIER(ID)
+    on delete cascade on update cascade,
+  type varchar (80) not null references WORK(type)
+  on delete cascade on update cascade,
+  date date not null,
+  primary key(sold_id,type)
+  
+);
+create table WAR (
+pincode varchar(6)  references LOCATION(pincode)
+on delete cascade on update cascade,
+status  int not null check (Status = 0 OR Status = 1 OR Status = 2),  /*0 for lost 1 for win 2 for undecisive*/
+date    int not null,
+strength int not null check (strength > 0),
+unique(date),
+primary key (pincode, date)
+);
+
+create table soldier_status (
+sold_id    int references SOLDIER(id)
+on update cascade on delete cascade,
+alive       int not null,
+wardate int not null,
+pincode varchar(6) not null,
+foreign key (wardate, pincode) references WAR(date, pincode)
+on update cascade on delete cascade,
+primary key (sold_id, wardate, pincode),
+unique(sold_id, wardate, pincode)
+);
+
+create table REGISTER (
+first_name varchar(100) not null,
+last_name varchar(100) not null,
+soldier_id int not null references SOLDIER(id),
+email varchar(100) not null,
+password varchar(100) not null check(char_length(password)>8),
+phone_no varchar(15) not null,
+soldier_rank varchar(10) not null references SOLDIER(soldier_rank),
+primary key(email)
+);
+
+
+
 describe LOCATION;
 describe BATALLIAN;
 describe SOLDIER;
 describe VISITED;
 describe MEDAL;
 describe REWARD;
-	
+describe MANUFACTURING;
+describe COMPANY;
+describe INVENTORY;
+describe WEAPONS;
+describe CATAGORY;
+describe WORK;
+describe ASSIGN;
+describe WAR;
+describe REGISTER;
+
+
+
+
 drop database military_management;
